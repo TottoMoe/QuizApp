a25cd7d907df1057303e153a1b7cab1833945e49
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2024-05-12 17:48:42 -0700

    first commit

---
+++ client/src/components/Main.js
@@ -0,0 +1,31 @@
+import React, {useRef} from "react";
+import { Link } from "react-router-dom";
+
+export default function Main() {
+
+  const inputRef = useRef(null);
+  return (
+    <div className="container">
+      <h1 className="title text-light">Quiz Application</h1>
+
+      <ol>
+        <li>You will be asked 10 questions one after antoher.</li>
+        <li>10 points is asarded fro the correct anser.</li>
+        <li>
+          Each question has three options. You can choose only one option is
+          correct.
+        </li>
+        <li>You can review and change answers before the quiz finish.</li>
+        <li>The result will be declared at the end of the quiz.</li>
+      </ol>
+
+      <form id="form">
+        <input ref={inputRef} type="text" placeholder="Username*"/>
+      </form>
+
+      <div className="start">
+        <Link className="btn" to={'quiz'}>Start Quiz</Link>
+      </div>
+    </div>
+  );
+}
